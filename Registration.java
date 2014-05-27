0d4855bbd7977ede4f4c1471df5b86b59867b85e
Kyle <uci.java@gmail.com>
2014-05-27 09:26:48 -0700

    Fixed Indentation in Registration Class
    In consistent use of tab versus spaces in Registration class caused
    issues when viewing in github.  This commit resolved the issue.

--- Registration.java
+++ Registration.java
@@ -9,163 +9,163 @@ import java.util.Collections;
 
 public class Registration {
 
-	static Scanner input = new Scanner(System.in);
-	
-	public static void main(String[] args) throws ParseException{
-		//  Declare Variables
-		ArrayList<Student> students = new ArrayList<Student>();
-		ArrayList<Course> courses = new ArrayList<Course>();
+    static Scanner input = new Scanner(System.in);
+    
+    public static void main(String[] args) throws ParseException{
+        //  Declare Variables
+        ArrayList<Student> students = new ArrayList<Student>();
+        ArrayList<Course> courses = new ArrayList<Course>();
 
-		int loginSelection;
-		int mainSelection;
-		Student currentStudent;
-		Student newStudent;
-		String logoutSelection = new String("N");
-		
-		//  Read Data  
-		//  TODO Call Util 
-		
-		
-		//  Test Classes  
-		//  REMOVE - once a seperate test class is written
-		students.add(0, new Student());
-		System.out.println("Registrating Main");
-		System.out.println(Util.test());
-		System.out.println(students.get(0).toString());
-		
-	    // Generate some a course and some class offerings
-		// REMOVE - once data is read from file
+        int loginSelection;
+        int mainSelection;
+        Student currentStudent;
+        Student newStudent;
+        String logoutSelection = new String("N");
+        
+        //  Read Data  
+        //  TODO Call Util 
+        
+        
+        //  Test Classes  
+        //  REMOVE - once a seperate test class is written
+        students.add(0, new Student());
+        System.out.println("Registrating Main");
+        System.out.println(Util.test());
+        System.out.println(students.get(0).toString());
+        
+        // Generate some a course and some class offerings
+        // REMOVE - once data is read from file
         ArrayList<CourseOffering> CS510Offerings = new ArrayList<CourseOffering>();
         CS510Offerings.add(new CourseOffering(new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH).parse("01/14/2014"), 
-        									  new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH).parse("05/23/2014"),
-        									  25, 
-        									  null,
-        									  null));
+                                              new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH).parse("05/23/2014"),
+                                              25, 
+                                              null,
+                                              null));
         CS510Offerings.add(new CourseOffering(new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH).parse("05/13/2014"), 
-				  		   					  new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH).parse("09/23/2014"),
-				  		   					  30, 
-				  		   					  null,
-				  		   					  null));
+                                                   new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH).parse("09/23/2014"),
+                                                   30, 
+                                                   null,
+                                                   null));
         
         
         courses.add(new Course("0001", 
-        					   "Intro to C++ Programming", 
-        					   510, 
-        					   "Provides an intro to computer programming using the C++ langugage.", 
-        					   "Computer Science", 
-        					   CS510Offerings));   
+                               "Intro to C++ Programming", 
+                               510, 
+                               "Provides an intro to computer programming using the C++ langugage.", 
+                               "Computer Science", 
+                               CS510Offerings));   
 
         System.out.println(courses.get(0).writeln());
         
 
         courses.add(new Course("CS123","Java I",32,"Introduction to Java","Computer Science",null));
-		System.out.println(courses.get(1).toString());
+        System.out.println(courses.get(1).toString());
 
 
-		//  Login Prompt
-		do{   
-			loginSelection = loginMenu();
-			if(loginSelection == 1){
-				currentStudent = studentLogin(students);
-				//  Main Menu Prompt
-				do{
-				    mainSelection = mainMenu();
-				    if (mainSelection == 1){
-				    	displayAvailableCourses(courses);
-				    }
-				    else if(mainSelection == 2){
-				    	displayEnrolledCourses(currentStudent,courses);
-				    }
-					else if(mainSelection == 3){
-						System.out.print("Please confirm you would like to logout (Y/N):  ");
-						logoutSelection = input.next();
-						System.out.println("");
-					}
-					else{
-						System.out.println("Invalid selection");
-					}
-				} while (!(mainSelection == 3 && logoutSelection.equals("Y")));
-			}
-			else if(loginSelection == 2){
-				newStudent = newUser(students);
-				if(newStudent != null){
-					students.add(newStudent);
-				}
-			}	
-			else if(loginSelection == 3){
-				System.out.println("Exiting the Registration System");
-			}
-			else {
-				System.out.println("Invalid selection");
-			}
-		} while (loginSelection != 3);
-		
-		//  Save Data
-		//  TODO Call Util
+        //  Login Prompt
+        do{   
+            loginSelection = loginMenu();
+            if(loginSelection == 1){
+                currentStudent = studentLogin(students);
+                //  Main Menu Prompt
+                do{
+                    mainSelection = mainMenu();
+                    if (mainSelection == 1){
+                        displayAvailableCourses(courses);
+                    }
+                    else if(mainSelection == 2){
+                        displayEnrolledCourses(currentStudent,courses);
+                    }
+                    else if(mainSelection == 3){
+                        System.out.print("Please confirm you would like to logout (Y/N):  ");
+                        logoutSelection = input.next();
+                        System.out.println("");
+                    }
+                    else{
+                        System.out.println("Invalid selection");
+                    }
+                } while (!(mainSelection == 3 && logoutSelection.equals("Y")));
+            }
+            else if(loginSelection == 2){
+                newStudent = newUser(students);
+                if(newStudent != null){
+                    students.add(newStudent);
+                }
+            }    
+            else if(loginSelection == 3){
+                System.out.println("Exiting the Registration System");
+            }
+            else {
+                System.out.println("Invalid selection");
+            }
+        } while (loginSelection != 3);
+        
+        //  Save Data
+        //  TODO Call Util
 
-	}
-	
-	private static int loginMenu(){
-		System.out.println("==================================");
-		System.out.println("Welcome to the Registration System");
-		System.out.println("Please select from the following:");
-		System.out.println("1. Login");
-		System.out.println("2. Request Access");
-		System.out.println("3. Exit");
-		System.out.print("Enter the number of you request:  "); 
-		int selection = input.nextInt();
-		System.out.println("");
-		return selection;
-	}
-	
-	private static int mainMenu(){
+    }
+    
+    private static int loginMenu(){
+        System.out.println("==================================");
+        System.out.println("Welcome to the Registration System");
+        System.out.println("Please select from the following:");
+        System.out.println("1. Login");
+        System.out.println("2. Request Access");
+        System.out.println("3. Exit");
+        System.out.print("Enter the number of you request:  "); 
+        int selection = input.nextInt();
+        System.out.println("");
+        return selection;
+    }
+    
+    private static int mainMenu(){
 
-		System.out.println("==================================");
-		System.out.println("Main Menu");
-		System.out.println("Please select from the following:");
-		System.out.println("1. Display all available courses");
-		System.out.println("2. Display all enrolled courses");
-		System.out.println("3. Logout");
-		System.out.print("Enter the number of you request:  "); 
-		int selection = input.nextInt();
-		System.out.println("");
-		return selection;
-	}
-	
-	
-	private static void displayAvailableCourses(ArrayList<Course> courses){
-		Collections.sort(courses, new courseCompare());
-		for(Course course : courses) {
-        	System.out.println(course.writeln());
-        	if(course.getCourseOfferings() != null){
-        	    for(CourseOffering offering : course.getCourseOfferings()) {
-        		    System.out.println(offering.writeln());
-        	    }
-			}
-	    }
-	}
-	
-	private static void displayEnrolledCourses(Student currentStudent,ArrayList<Course> courses){
-		System.out.println("Display Enrolled Courses");
-		System.out.println(currentStudent.toString());
-		displayAvailableCourses(courses);
-	//  TODO ADD STUDENT LOGIN 
-	}
-	
-	
-	private static Student studentLogin(ArrayList<Student> students){
-		System.out.println("Student Login");
-		return students.get(0);
-	//  TODO ADD STUDENT LOGIN 
-	}
-	
-	
-	private static Student newUser(ArrayList<Student> students){
-		Student newStudent = null;
-		System.out.println("Request new user access");
-		return newStudent;
-	//  TODO ADD NEW USER ACCESS
-	}
+        System.out.println("==================================");
+        System.out.println("Main Menu");
+        System.out.println("Please select from the following:");
+        System.out.println("1. Display all available courses");
+        System.out.println("2. Display all enrolled courses");
+        System.out.println("3. Logout");
+        System.out.print("Enter the number of you request:  "); 
+        int selection = input.nextInt();
+        System.out.println("");
+        return selection;
+    }
+    
+    
+    private static void displayAvailableCourses(ArrayList<Course> courses){
+        Collections.sort(courses, new courseCompare());
+        for(Course course : courses) {
+            System.out.println(course.writeln());
+            if(course.getCourseOfferings() != null){
+                for(CourseOffering offering : course.getCourseOfferings()) {
+                    System.out.println(offering.writeln());
+                }
+            }
+        }
+    }
+    
+    private static void displayEnrolledCourses(Student currentStudent,ArrayList<Course> courses){
+        System.out.println("Display Enrolled Courses");
+        System.out.println(currentStudent.toString());
+        displayAvailableCourses(courses);
+    //  TODO ADD STUDENT LOGIN 
+    }
+    
+    
+    private static Student studentLogin(ArrayList<Student> students){
+        System.out.println("Student Login");
+        return students.get(0);
+    //  TODO ADD STUDENT LOGIN 
+    }
+    
+    
+    private static Student newUser(ArrayList<Student> students){
+        Student newStudent = null;
+        System.out.println("Request new user access");
+        return newStudent;
+    //  TODO ADD NEW USER ACCESS
+    }
 
 }
 //  Create course comparison class to compare by course name
